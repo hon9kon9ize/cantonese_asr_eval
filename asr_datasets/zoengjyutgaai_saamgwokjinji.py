5218cfed7819e37eb86305193dbbd7d526654890
Joseph Cheng <indiejoseph@gmail.com>
2025-01-24 17:32:14 +0000

    add ngram processor

--- asr_datasets/zoengjyutgaai_saamgwokjinji.py
+++ asr_datasets/zoengjyutgaai_saamgwokjinji.py
@@ -1,19 +1,25 @@
 from datasets import load_dataset
 from datasets import Dataset
 
+
 class ZoengjyutgaaiSaamgwokjinjiDataset(Dataset):
-    def __init__(self, dataset_path="laubonghaudoi/zoengjyutgaai_saamgwokjinji", split='saamgwokjinji+seoiwuzyun', batch_size=64):
+    def __init__(
+        self,
+        dataset_path="laubonghaudoi/zoengjyutgaai_saamgwokjinji",
+        split="saamgwokjinji+seoiwuzyun",
+        batch_size=64,
+    ):
         self.dataset = load_dataset(dataset_path, split=split)
         self.batch_size = batch_size
         self.audio_paths = list(self.dataset)
-    
+
     def __iter__(self):
         for i in range(0, len(self.audio_paths), self.batch_size):
-            batch = self.audio_paths[i:i + self.batch_size]
-            batch_audios = [sample['audio'] for sample in batch]
-            batch_sentences = [sample['transcription'] for sample in batch]
+            batch = self.audio_paths[i : i + self.batch_size]
+            batch_audios = [sample["audio"] for sample in batch]
+            batch_sentences = [sample["transcription"] for sample in batch]
             yield batch_audios, batch_sentences
-    
+
     def __len__(self):
         return len(self.audio_paths)
 
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from asr_models.sensevoice_model import SenseVoiceASRModel
-from asr_models.whisper_model import WhisperASRModel
+from asr_models.whisper_model import WhisperASRModel, WhisperASRModelWithNgram
 from asr_datasets.common_voice import CommonVoiceDataset
 from asr_datasets.guangzhou_daily_use import GuangzhouDailyUseDataset
 from asr_datasets.guangzhou_cabin import GuangzhouCabinDataset
@@ -12,12 +12,18 @@ import json
 import os
 from tqdm import tqdm
 
-device = ("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else ("mps" if torch.backends.mps.is_available() else "cpu")
+)
 batch_size = 50
-num_models = 3
+num_models = 4
 num_datasets = 6
 
 for dataset_index in range(num_datasets):
+    if dataset_index == 0:
+        continue
     if dataset_index == 0:
         dataset = ZoengjyutgaaiSaamgwokjinjiDataset(batch_size=batch_size)
     elif dataset_index == 1:
@@ -30,36 +36,60 @@ for dataset_index in range(num_datasets):
         dataset = WordshkHiujinDataset(batch_size=batch_size)
     elif dataset_index == 5:
         dataset = MixedCantoneseAndEnglishDataset(batch_size=batch_size)
-    
+
     dataset_name = dataset.get_name()
 
     for model_index in range(num_models):
         if model_index == 0:
             model = SenseVoiceASRModel(device=device)
         elif model_index == 1:
-            model = WhisperASRModel(model_name="alvanlii/whisper-small-cantonese", device=device)
+            model = WhisperASRModel(
+                model_name="alvanlii/whisper-small-cantonese", device=device
+            )
         elif model_index == 2:
-            model = WhisperASRModel(model_name="Scrya/whisper-large-v2-cantonese", device=device)
-        
+            model = WhisperASRModel(
+                model_name="Scrya/whisper-large-v2-cantonese", device=device
+            )
+        elif model_index == 3:
+            model = WhisperASRModelWithNgram(
+                model_name="Scrya/whisper-large-v2-cantonese",
+                lm_model="words.text_correct.arpa",
+                device=device,
+            )
+
         model_name = model.get_name()
 
         # Skip if the result file already exists
-        if os.path.exists(f'results/{model_name}/{dataset_name}.json'):
-            print(f"Results for {model_name} on {dataset_name} already exist. Skipping...")
+        if os.path.exists(f"results/{model_name}/{dataset_name}.json"):
+            print(
+                f"Results for {model_name} on {dataset_name} already exist. Skipping..."
+            )
             continue
 
         results = []
-        for batch_audios, batch_sentences in tqdm(dataset, desc=f"{model_name} on {dataset_name}", total=len(dataset)//batch_size):
-            transcriptions = model.generate([
-                librosa.resample(audio['array'], orig_sr=audio['sampling_rate'], target_sr=16000)
-                for audio in batch_audios
-            ])
+        for batch_audios, batch_sentences in tqdm(
+            dataset,
+            desc=f"{model_name} on {dataset_name}",
+            total=len(dataset) // batch_size,
+        ):
+            transcriptions = model.generate(
+                [
+                    librosa.resample(
+                        audio["array"], orig_sr=audio["sampling_rate"], target_sr=16000
+                    )
+                    for audio in batch_audios
+                ]
+            )
             for transcription, sentence in zip(transcriptions, batch_sentences):
-                results.append({"transcription": transcription["text"], "expected": sentence})
+                results.append(
+                    {"transcription": transcription["text"], "expected": sentence}
+                )
 
         # Create directory if it doesn't exist
-        os.makedirs(f'results/{model_name}', exist_ok=True)
-        
+        os.makedirs(f"results/{model_name}", exist_ok=True)
+
         # Save results to a JSON file
-        with open(f'results/{model_name}/{dataset_name}.json', 'w', encoding='utf-8') as f:
+        with open(
+            f"results/{model_name}/{dataset_name}.json", "w", encoding="utf-8"
+        ) as f:
             json.dump(results, f, ensure_ascii=False, indent=4)
